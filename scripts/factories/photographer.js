c2a1a00c82e82c127f22a636543f034a59940abb
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-07-19 15:05:41 +0200

    apply styles to imported data on home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,25 +1,31 @@
 export const photographerFactory = (data) => {
-    const { name, portrait, city, country } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        // creation part
-        const article = document.createElement( 'article' ); // <article></article>
+        // creating elements
+        const article = document.createElement( 'article' ); 
         const img = document.createElement( 'img' );
-        const p = document.createElement('p') // <p></p>
+        const h3 = document.createElement('h3');
+        const h4 = document.createElement('h4');
+        const h5 = document.createElement('h5');
 
-        // populate part
-        img.setAttribute("src", picture) // <image src=picture />
+        // populating elements
+        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        p.textContent = `${city}, ${country}`
+        h3.textContent = `${city}, ${country}`
+        h4.textContent = tagline;
+        h5.textContent = `$${price}/day`;
 
 
-        // ordering part
+        // ordering elements
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(p)
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(h5);
 
         return (article);
 
