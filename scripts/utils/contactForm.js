af1c4563ea2eb22530264abe5edf135cd0a5b3fa
Georgina Mudditt <g.k.mudditt@gmail.com>
2024-08-19 14:13:33 +0200

    working on contact form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -26,3 +26,31 @@ document.getElementById('contactFormLabel').addEventListener('click', handleCont
 // calling the functions
 displayModal();
 closeModal();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const contactButton = document.getElementById('contactButton');
+    if (contactButton) {
+      contactButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        closeModal();
+        const thankYouMessage = document.getElementById('thankYouMessage');
+        thankYouMessage.style.display = 'block';
+        document.querySelector('.photograph-header').classList.add('blurred-background');
+        document.querySelector('.media-content').classList.add('blurred-background');
+        document.querySelector('.likes-box').classList.add('blurred-background');
+        document.querySelector('.logo').classList.add('blurred-background');
+      });
+    }
+
+    const closeIcon = document.querySelector('.close-icon');
+    if (closeIcon) {
+      closeIcon.addEventListener('click', function() {
+        const thankYouMessage = document.getElementById('thankYouMessage');
+        thankYouMessage.style.display = 'none';
+        document.querySelector('.photograph-header').classList.remove('blurred-background');
+        document.querySelector('.media-content').classList.remove('blurred-background');
+        document.querySelector('.likes-box').classList.remove('blurred-background');
+        document.querySelector('.logo').classList.remove('blurred-background');
+      });
+    }
+});
